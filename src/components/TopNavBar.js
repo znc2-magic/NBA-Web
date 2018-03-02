029f65c7f99829bb598f0bc714d20cbf581cfe5d
Shane <znc2.magic@gmail.com>
2018-03-01 23:42:30 -0800

    Add positioned NBA logo and css file for topnavbar

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from '../assets/image/logo.svg';
+import logo from '../assets/image/nba-logoman-word-white.svg';
 
 export class TopNavBar extends React.Component {
   render() {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
       </header>
     )
   }
