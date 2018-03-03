487a5865bf30ef22508e9144bcd6b300999980f2
Shane <znc2.magic@gmail.com>
2018-03-02 20:18:33 -0800

    finalized main page UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,14 +6,27 @@ import nba from 'nba';
 export class Main extends React.Component {
   state = {
     playerId: nba.findPlayer('Stephen Curry').playerId,
+    playerInfo: {},
   };
+
+  componentDidMount() {
+    nba.stats.playerInfo({PlayerID: this.state.playerId}).then((info) => {
+      const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+      this.setState({playerInfo: playerInfo});
+      console.log(playerInfo);
+    })
+
+  }
 
   render() {
     return (
       <div className="main">
-        <Profile/>
+        <Profile playerInfo={this.state.playerInfo}/>
         <ShotChart playerId={this.state.playerId}/>
       </div>
     );
   }
 }
+
+//https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/1610612745/2017/260x190/201935.png
+//https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/1610612739/2017/260x190/2544.png
